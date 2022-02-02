c6e6f9027c478a601fa62cceec7f9723eb1d38c6
rattanmanna <rattanma@sheridancollege.ca>
2022-02-02 11:31:34 -0500

    edit done

--- CardCode-master/src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
+++ CardCode-master/src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
@@ -1,19 +1,19 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Name = Manvir Singh Rattan
+ * Student id= 991658419
  */
 package ca.sheridancollege.week2.softwarefundamentals;
 
 /**
  * The class where we create Cards
- * @author dancye, 2019
+ * @author Manvir Singh Rattan , 2022
  */
 public class CardDealer 
 {
     public static void main(String[] args)
     {
-        Card nineClubs = new Card("Clbs", 9);
-        nineClubs.setSuit("diamonds");
+        Card ace = new Card("ace", 6);
+        ace.setSuit("diamonds");
+        System.out.println("ace suit"+ace.getSuit());
     }
 }
